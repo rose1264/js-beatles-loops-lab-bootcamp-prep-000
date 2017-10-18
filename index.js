42943f7b1e7a510c61c5e22ac7110101c3f140df
Shumei Huang <rose1264@gmail.com>
2017-10-18 02:35:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+  for (var i=0;i<musicians.length;i++){
+    emptyArray.push(musicians[i] + ' plays ' +instruments[i] )
+  };
+  return emptyArray;
+}
